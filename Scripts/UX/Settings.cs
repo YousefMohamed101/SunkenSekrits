7677f22a1eb76bb286aad1e26c395fc327d63358
YousefMohamed101 <thehappyboy28@gmail.com>
2024-12-23 01:02:15 +0800

    0.2.2

--- Scripts/UX/Settings.cs
+++ Scripts/UX/Settings.cs
@@ -14,17 +14,23 @@ public partial class Settings : Control
     private HSlider hsBGMVolume;
     private HSlider hsSFXVolume;
     private HSlider hsFrameLimit;
+    private HSlider hsMouseSensitivity;
     private bool bIsAudioPlaying = false;
     private SettingData sdSettingData;
     private OptionButton obWindowMode;
     private OptionButton obResolution;
+    private OptionButton obVSYNCMode;
     private Label lblMasterVolume;
     private Label lblVoiceVolume;
     private Label lblBGMVolume;
     private Label lblSFXVolume;
+    private Label lblFrameLimit;
+    private Label lblMouseSensitivity;
     private Label[] lblAudioLabels;
     public override void _Ready()
     {
+        
+        //Audio tab Get Nodes
         lblMasterVolume =GetNode<Label>("CenterContainer/VBoxContainer/TabContainer/Audio/MarginContainer/AudioContolBoard/MasterVolumeField/HBoxContainer2/MasterCounter");
         lblVoiceVolume = GetNode<Label>("CenterContainer/VBoxContainer/TabContainer/Audio/MarginContainer/AudioContolBoard/VoiceVolumeField/HBoxContainer2/VoiceCounter");
         lblBGMVolume = GetNode<Label>("CenterContainer/VBoxContainer/TabContainer/Audio/MarginContainer/AudioContolBoard/BGMVolumeField/HBoxContainer2/Counter");
@@ -34,18 +40,44 @@ public partial class Settings : Control
         hsVoiceVolume = GetNode<HSlider>("%VoiceVolumeSlider");
         hsBGMVolume = GetNode<HSlider>("%BGMVolumeSlider");
         hsSFXVolume = GetNode<HSlider>("%SFXVolumeSlider");
-        aspUISFX = GetNode<AudioStreamPlayer>("%SettingUISFX");
-        btnBack = GetNode<Button>("%SettingsBack");
-        obWindowMode = GetNode<OptionButton>("%WindowModeSelector");
-        obResolution = GetNode<OptionButton>("%ResolutionSelector");
-        hsFrameLimit = GetNode<HSlider>("%FrameLimitSlider");
         
-        btnBack.Pressed += ()=> EmitSignal("SettingReturn");
+        //Audio tab Signal connect
         hsMasterVolume.ValueChanged += Value => AudioSlidersChange(Value,0);
         hsVoiceVolume.ValueChanged += Value => AudioSlidersChange(Value,1);
         hsBGMVolume.ValueChanged += Value => AudioSlidersChange(Value,2);
         hsSFXVolume.ValueChanged += Value => AudioSlidersChange(Value,3);
+        
+        
+        //Game tab Get Nodes
+        lblMouseSensitivity = GetNode<Label>("CenterContainer/VBoxContainer/TabContainer/Game/MarginContainer/VBoxContainer/MouseSensetivityField/HBoxContainer/Counter");
+        hsMouseSensitivity = GetNode<HSlider>("%MouseSesitivitySlider");
+        
+        //Game tab signal connect
+        hsMouseSensitivity.ValueChanged += SetMouseSensitivity;
+        
+        
+        //Graphic tab Get Nodes
+        obVSYNCMode = GetNode<OptionButton>("%VSyncSelector");
+        obWindowMode = GetNode<OptionButton>("%WindowModeSelector");
+        obResolution = GetNode<OptionButton>("%ResolutionSelector");
+        hsFrameLimit = GetNode<HSlider>("%FrameLimitSlider");
+        lblFrameLimit = GetNode<Label>("CenterContainer/VBoxContainer/TabContainer/Graphic/MarginContainer/GridContainer/HBoxContainer/Counter");
+        
+       //Graphic tab Signal connection
         hsFrameLimit.ValueChanged += SetFrameLimit;
+        obVSYNCMode.ItemSelected += SetVSync;
+        obWindowMode.ItemSelected += SetWindowMode;
+        
+        //Setting General function Get Nodes
+        aspUISFX = GetNode<AudioStreamPlayer>("%SettingUISFX");
+        btnBack = GetNode<Button>("%SettingsBack");
+        
+        //Setting General function Signal Connection
+        btnBack.Pressed += ()=> EmitSignal("SettingReturn");
+        
+        
+        
+        
     }
     
     public void Open()
@@ -69,11 +101,55 @@ public partial class Settings : Control
     private void SetFrameLimit(double value)
     {
         Engine.MaxFps = (int)value;
+        lblFrameLimit.Text = Engine.MaxFps.ToString();
     }
-    private void SeVSync(int Index)
+    private void SetVSync(long Index)
     {
+
+        switch (Index)
+        {
+            case 0:
+                DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Disabled);
+                break;
+            case 1:
+                DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Enabled);
+                break;
+            case 2:
+                DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Adaptive);
+                break;
+            default:
+                DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Disabled);
+                break;
+        }
         
-            DisplayServer.WindowSetVsyncMode(DisplayServer.VSyncMode.Enabled);
+        
+        
+
+    }
+
+    private void SetMouseSensitivity(double value)
+    {
+        lblMouseSensitivity.Text = (Mathf.Floor(value*100f)/100f).ToString();
+    }
+
+    private void SetWindowMode(long Index)
+    {
+        switch (Index)
+        {
+            case 0:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+                break;
+            case 1:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.Windowed);
+                break;
+            default:
+                DisplayServer.WindowSetMode(DisplayServer.WindowMode.Fullscreen);
+                break;
+        }
+    }
+
+    private void SetResolution(long value)
+    {
         
     }
 }
